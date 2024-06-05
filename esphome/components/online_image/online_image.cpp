a4c8f9ce79c9d7cf7a57f09dc1ea7bc6cf1341ab
Thijs Dohmen <t.dohmen@zuyderland.nl>
2024-06-05 15:05:22 +0200

    test

--- esphome/components/online_image/online_image.cpp
+++ esphome/components/online_image/online_image.cpp
@@ -354,7 +354,7 @@ void OnlineImage::update() {
     ESP_LOGI(TAG, "Updating image");
   }
 
-  WiFiClient client = http_.getClient();
+  WiFiClient client;
   int begin_status = http_.begin( client , url_);
   if (!begin_status) {
     ESP_LOGE(TAG, "Could not download image from %s. Connection failed: %i", url_, begin_status);
